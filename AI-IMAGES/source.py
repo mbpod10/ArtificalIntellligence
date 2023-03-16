58396575446dbc61fa66c8fbb76ae84a04d6fedc
M. Brock Podgurski <mbpod10@gmail.com>
2023-03-16 11:11:27 -0400

    change README

--- AI-IMAGES/source.py
+++ AI-IMAGES/source.py
@@ -7,17 +7,16 @@ def configure():
     load_dotenv()
 
 
-configure()
+def generate_random_image(payload):
+    configure()
+    url = "https://api.tryleap.ai/api/v1/images/models/8b1b897c-d66d-45a6-b8d7-8e32421d02cf/inferences"
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {os.getenv('api_key')}"
+    }
+    response = requests.post(url, json={"prompt": payload}, headers=headers)
+    print(response.text)
 
-url = "https://api.tryleap.ai/api/v1/images/models/8b1b897c-d66d-45a6-b8d7-8e32421d02cf/inferences"
 
-payload = {"prompt": "A cat in cyberpunk world"}
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "authorization": f"Bearer {os.getenv('api_key')}"
-}
-
-response = requests.post(url, json=payload, headers=headers)
-
-print(response.text)
+print(generate_random_image("Blackbeard on the Queen Anne's Revenge"))
---
+++ REAMDE.md
@@ -0,0 +1,5 @@
+# AI-Generated Images With Leap
+
+![blackbeard](AI-IMAGES/Gen-Images/blackBeard.png)
+![croc](AI-IMAGES/Gen-Images/crocOnNile.png)
+![cat](AI-IMAGES/Gen-Images/militaryCat.png)
